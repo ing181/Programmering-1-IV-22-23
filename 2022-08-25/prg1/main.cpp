e252d63934e5215b8a6045b00cdbd4470847bccd
ing181 <ingcei0123@edu.lerum.se>
2022-08-25 16:01:05 +0200

    Dag 2

---
+++ 2022-08-25/prg1/main.cpp
@@ -0,0 +1,23 @@
+// Programmet använder sig av
+// programkoden (bibliotek)
+// i "iostream"
+#include <iostream>
+using namespace std;
+// int = integer (heltal)
+int main()
+{
+    // cout = skärmen
+    // << = utmatningsoperatorn
+    // HEJ = det som matas ut
+    // << = utmatningsoperatorn
+    // endl = ny rad
+    // ; = slut på satsen
+    cout << "HEJ" << endl;
+   // cout << 7/0 << endl; // Här blir det fel
+    // return (skicka tillbaka, returnera)
+    // heltalet 0 till operativsystemet.
+    // Om något går fel, kommer aldrig programmet
+    // till rad 21 utan ett annat heltal att
+    // skickas till operativsystemet.
+    return 0;
+}
