82fe1b369853d059595102f2fd753b465ce1ebf8
ing181 <ingcei0123@edu.lerum.se>
2022-08-30 09:03:15 +0200

    Lektion

---
+++ 2022-08-30/felmeddelande/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Mata in ett tal" << endl;
+    // En variabel för heltal skapas
+    // variabel deklareras
+    int tal;
+    // Från tangentborde till "tal"
+    cin >> tal;
+    // Innehållet i variabeln tal
+    // matas ut till skärmen
+    cout << "Du matade in " << tal << endl;
+    // räkneoperatorer +, -, *, /
+    cout << "plus 4 blir det " << (tal+4) << endl;
+    // Variabel för heltal: int
+    // Variabel för flyttal (decimaltal) float
+    return 0;
+}
---
+++ 2022-08-30/string/main.cpp
@@ -0,0 +1,13 @@
+/* Online C++ compiler to run C++ program online
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string namn;
+cout << "Vad heter du? ";
+cin >> namn;
+cout << "Mitt namn är " << namn;
+
+    return 0;
+}
