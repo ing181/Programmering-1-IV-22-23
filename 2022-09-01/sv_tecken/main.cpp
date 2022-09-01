e58555914f3f2f5d9b073b735af2bfc66fdda563
ing181 <ingcei0123@edu.lerum.se>
2022-09-01 16:47:18 +0200

    chcp

---
+++ 2022-09-01/sv_tecken/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Windows "Command Promt" klarar bara
+    // engelska bokstäver.
+    // Vi måste tvinga den att byta teckenkodning
+    system("chcp 1252");
+    // Om du inte vill att bytet ska synas i ditt program lägg till
+    system("cls");
+    cout << "Hej ÅÄÖ åäö" << endl;
+    // "system" betyder att ett Windowsprogram ska köras. Ex: Change Codepage "chsp"
+    // eller "cls" Clear Screen
+    return 0;
+}
