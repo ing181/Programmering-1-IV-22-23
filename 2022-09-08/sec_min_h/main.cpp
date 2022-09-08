b8ed3ed3ee24f220cf7498db1770fa90b1ea35c5
ing181 <ingcei0123@edu.lerum.se>
2022-09-08 17:19:28 +0200

    Lektion

--- 2022-09-08/sec_min_h/main.cpp
+++ 2022-09-08/sec_min_h/main.cpp
@@ -6,21 +6,24 @@ Använd endast heltalsvariabler.
 using namespace std;
 int main()
 {
-    // int min_alder; // snake case
-    // int minAlder; // Camel case
-    // cout << minAlder << " " << mina_lder << endl;
-    // Variabel som inte gets något värde innehåller ett godtyckligt värde
-
     cout << "Ange antalet sekunder: ";
     int sekunder;
+    // Användaren matar in antalet sekunder
     cin >> sekunder;
 	int timmar;
+	// I "timmar" sparas antalet hela timmar
 	timmar = sekunder / 3600;
-	int rest_sekunder = sekunder % 3600;
+	// i "rest_sekunder" sparas de sekunder som blivit kvar när
+	// antalet hela timmar har tagits bort, 3600 sekunder för varje hel timme
+    int rest_sekunder;
+    rest_sekunder = sekunder % 3600;
 	int minuter;
+	// I "minuter" sparas antalet hela minuter
 	minuter = rest_sekunder / 60;
-	sekunder = rest_sekunder % 60;
-    cout << sekunder  << " sekunder, " << minuter << " minuter och " << timmar << " timmar " << endl;
-
+	// i "rest_sekunder2" sparas de sekunder som blivit kvar när
+	// antalet hela minuter har tagits bort, 60 sekunder för varje hel timme
+	int rest_sekunder2;
+	rest_sekunder2 = rest_sekunder % 60;
+    cout << timmar << " timmar, " << minuter << " minuter och " << rest_sekunder2 << " sekunder" << endl;
     return 0;
 }
