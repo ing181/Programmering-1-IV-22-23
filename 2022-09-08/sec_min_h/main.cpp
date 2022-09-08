c4f59292f7527e34809ae4d0932ed098096d8a98
ing181 <ingcei0123@edu.lerum.se>
2022-09-08 17:20:38 +0200

    Lektion

--- 2022-09-08/sec_min_h/main.cpp
+++ 2022-09-08/sec_min_h/main.cpp
@@ -10,20 +10,20 @@ int main()
     int sekunder;
     // Användaren matar in antalet sekunder
     cin >> sekunder;
-	int timmar;
-	// I "timmar" sparas antalet hela timmar
-	timmar = sekunder / 3600;
-	// i "rest_sekunder" sparas de sekunder som blivit kvar när
-	// antalet hela timmar har tagits bort, 3600 sekunder för varje hel timme
+    int timmar;
+    // I "timmar" sparas antalet hela timmar
+    timmar = sekunder / 3600;
+    // i "rest_sekunder" sparas de sekunder som blivit kvar när
+    // antalet hela timmar har tagits bort, 3600 sekunder för varje hel timme
     int rest_sekunder;
     rest_sekunder = sekunder % 3600;
-	int minuter;
-	// I "minuter" sparas antalet hela minuter
-	minuter = rest_sekunder / 60;
-	// i "rest_sekunder2" sparas de sekunder som blivit kvar när
-	// antalet hela minuter har tagits bort, 60 sekunder för varje hel timme
-	int rest_sekunder2;
-	rest_sekunder2 = rest_sekunder % 60;
+    int minuter;
+    // I "minuter" sparas antalet hela minuter
+    minuter = rest_sekunder / 60;
+    // i "rest_sekunder2" sparas de sekunder som blivit kvar när
+    // antalet hela minuter har tagits bort, 60 sekunder för varje hel timme
+    int rest_sekunder2;
+    rest_sekunder2 = rest_sekunder % 60;
     cout << timmar << " timmar, " << minuter << " minuter och " << rest_sekunder2 << " sekunder" << endl;
     return 0;
 }
