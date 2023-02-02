cf6c071f6d3f61b4ba033744f8a7f197099c44f3
ing181 <ingcei0123@edu.lerum.se>
2023-02-02 10:29:36 +0100

    w3schools

---
+++ 2023-02-02/w3schools/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void myFunction(int myNumbers[]) {
+  for (int i = 0; i < 5; i++) {
+    cout << myNumbers[i] << "\n";
+  }
+}
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  myFunction(myNumbers);
+  return 0;
+}
